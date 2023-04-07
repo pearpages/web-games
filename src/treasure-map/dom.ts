b624d1ef57a3ed03f9ecee0523aa4d849b4480c3
Pere Pages <pere.pages@ocado.com>
2023-04-07 17:30:57 +0200

    refactor: dom

--- src/treasure-map/dom.ts
+++ src/treasure-map/dom.ts
@@ -1,14 +1,14 @@
 type CreateElement = (doMagic: (div: HTMLDivElement) => void) => HTMLDivElement;
 
 const createDiv: CreateElement = (doMagic) => {
-    const div = document.createElement('div');
-    doMagic(div);
-    return div;
+  const div = document.createElement("div");
+  doMagic(div);
+  return div;
+};
+
+function append(element: HTMLElement) {
+  document.body.appendChild(element);
 }
 
-function render(element: HTMLElement) {
-    document.body.appendChild(element);
-}
-
-export { createDiv, render };
-export type { CreateElement }
+export default { createDiv, append };
+export type { CreateElement };
--- src/treasure-map/treasure-map.ts
+++ src/treasure-map/treasure-map.ts
@@ -1,91 +1,115 @@
-import createMapFactory from './map';
-import createHeatMap from './heat-map';
-import { createDiv, render } from './dom';
-import createClickFactory from './click';
-import type { Point, Event } from './models';
-import { getRandomPoint, getTemperature } from './math';
+import createMapFactory from "./map";
+import createHeatMap from "./heat-map";
+import DOM from "./dom";
+import createClickFactory from "./click";
+import type { Point, Event } from "./models";
+import { getRandomPoint, getTemperature } from "./math";
 
 const options = {
-    zoom: 6,
-    size: 50,
-    toScreen({ x, y }: Point, { x: offsetX, y: offsetY } = { x: 0, y: 0 }): Point {
-        if (x === 0) {
-            return { x: x + offsetX, y: (y * this.zoom) + offsetY };
-        }
-        if (y === 0) {
-            return { x: (x * this.zoom) + offsetX, y: y + offsetY };
-        }
-        return { x: (x * this.zoom) + offsetX, y: (y * this.zoom) + offsetY };
-    },
-    normalize({ x, y }: Point): Point {
-        return { x: Math.floor(x / this.zoom), y: Math.floor(y / this.zoom) }
-    },
-    toPoint(event: Event): Point {
-        return {
-            x: event.offsetX,
-            y: event.offsetY
-        };
+  zoom: 6,
+  size: 50,
+  toScreen(
+    { x, y }: Point,
+    { x: offsetX, y: offsetY } = { x: 0, y: 0 }
+  ): Point {
+    if (x === 0) {
+      return { x: x + offsetX, y: y * this.zoom + offsetY };
     }
-}
+    if (y === 0) {
+      return { x: x * this.zoom + offsetX, y: y + offsetY };
+    }
+    return { x: x * this.zoom + offsetX, y: y * this.zoom + offsetY };
+  },
+  normalize({ x, y }: Point): Point {
+    return { x: Math.floor(x / this.zoom), y: Math.floor(y / this.zoom) };
+  },
+  toPoint(event: Event): Point {
+    return {
+      x: event.offsetX,
+      y: event.offsetY,
+    };
+  },
+};
 
 function showHeatMapButton(onclick: (removeButton: () => void) => void): void {
-    const button = document.createElement('button');
-    button.innerText = 'Show heat map';
-    button.onclick = () => onclick(() => button.remove());
-    document.body.appendChild(button);
+  const button = document.createElement("button");
+  button.innerText = "Show heat map";
+  button.onclick = () => onclick(() => button.remove());
+  document.body.appendChild(button);
 }
 
 export default function game() {
-    const createClick = createClickFactory({ create: createDiv, size: options.zoom });
-    const createMap = createMapFactory(createDiv);
+  const createClick = createClickFactory({
+    create: DOM.createDiv,
+    size: options.zoom,
+  });
+  const createMap = createMapFactory(DOM.createDiv);
 
-    const treasure = getRandomPoint({ right: options.size, bottom: options.size });
+  const treasure = getRandomPoint({
+    right: options.size,
+    bottom: options.size,
+  });
 
-    let won = false;
+  let won = false;
 
-    const score = {
-        id: 'score',
-        totalClicks: 0,
-        getScoreText() {
-            return `Total clicks: ${this.totalClicks}`
-        },
-        init() {
-            render(createDiv(div => {
-                div.id = this.id;
-                div.innerText = this.getScoreText();
-            }))
-        },
-        reRender() {
-            document.getElementById(this.id).innerText = this.getScoreText();
-        }
-    };
+  const score = {
+    id: "score",
+    totalClicks: 0,
+    getScoreText() {
+      return `Total clicks: ${this.totalClicks}`;
+    },
+    init() {
+      DOM.append(
+        DOM.createDiv((div) => {
+          div.id = this.id;
+          div.innerText = this.getScoreText();
+        })
+      );
+    },
+    reRender() {
+      document.getElementById(this.id).innerText = this.getScoreText();
+    },
+  };
 
-    const map = createMap({
-        size: options.size * options.zoom, onClick: (event: Event) => {
-            if (won) {
-                return;
-            }
-            const clickedPoint = options.normalize(options.toPoint(event));
-            score.totalClicks++;
-            if (treasure.x === clickedPoint.x && treasure.y === clickedPoint.y) {
-                alert('You found the treasure!');
-                won = true;
-                render(createDiv(div => {
-                    div.innerText = 'You won!';
-                    div.style.fontSize = '30px';
-                    div.style.color = 'red';
-                }));
-            }
-            const temperature = getTemperature(treasure, clickedPoint, options.zoom);
-            render(createClick({ point: options.toScreen(clickedPoint), temperature }).getElement());
-            score.reRender();
-        }
+  const map = createMap({
+    size: options.size * options.zoom,
+    onClick: (event: Event) => {
+      if (won) {
+        return;
+      }
+      const clickedPoint = options.normalize(options.toPoint(event));
+      score.totalClicks++;
+      if (treasure.x === clickedPoint.x && treasure.y === clickedPoint.y) {
+        alert("You found the treasure!");
+        won = true;
+        DOM.append(
+          DOM.createDiv((div) => {
+            div.innerText = "You won!";
+            div.style.fontSize = "30px";
+            div.style.color = "red";
+          })
+        );
+      }
+      const temperature = getTemperature(treasure, clickedPoint, options.zoom);
+      DOM.append(
+        createClick({
+          point: options.toScreen(clickedPoint),
+          temperature,
+        }).getElement()
+      );
+      score.reRender();
+    },
+  });
+  DOM.append(map.getElement());
+  score.init();
+  showHeatMapButton((removeButton: () => void) => {
+    createHeatMap({
+      options,
+      treasure,
+      render: DOM.append,
+      createClick,
+      offset: { x: 301, y: 0 },
     });
-    render(map.getElement());
-    score.init();
-    showHeatMapButton((removeButton: () => void) => {
-        createHeatMap({ options, treasure, render, createClick, offset: { x: 301, y: 0 } });
-        removeButton()
-    });
-
+    removeButton();
+  });
 }
