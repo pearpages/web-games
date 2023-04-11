e1b49e293af7a8ea94991c7defeb84f7e6f14223
Pere Pages <pere.pages@ocado.com>
2023-04-11 19:05:49 +0200

    refactor: setup

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,64 @@
-import helloWorld from './hello-world';
-import treasureMap from './treasure-map';
-import canvasBasics from './canvas-basics';
-import canvasMovement from './canvas-movement';
-import canvasKeyboard from './canvas-keyboard';
-import snake from './snake';
+import helloWorld from "./hello-world";
+import treasureMap from "./treasure-map";
+import canvasBasics from "./canvas-basics";
+import canvasMovement from "./canvas-movement";
+import canvasKeyboard from "./canvas-keyboard";
+import snake from "./snake";
+import platformGame from "./platform-game-dom";
 
 function clean() {
-    document.body.innerHTML = '';
+  document.body.innerHTML = "";
 }
 
 const routeNames = [
-    'hello world',
-    'treasure map',
-    'canvas basics',
-    'canvas movement',
-    'canvas keyboard',
-    'snake'
+  "hello world",
+  "treasure map",
+  "canvas basics",
+  "canvas movement",
+  "canvas keyboard",
+  "snake",
+  "html platform game",
 ];
 
 function createHref(textContent: string) {
-    return `/${textContent.toLowerCase().replace(' ', '-')}`
+  return `/${textContent.toLowerCase().replace(/ /g, "-")}`;
 }
 
 function createLink(textContent: string) {
-    const link = document.createElement('a');
-    link.textContent = textContent;
-    link.style.display = 'block';
-    link.href = createHref(textContent);
-    return link;
+  const link = document.createElement("a");
+  link.textContent = textContent;
+  link.style.display = "block";
+  link.href = createHref(textContent);
+  return link;
 }
 
-routeNames.forEach(name => document.body.appendChild(createLink(name)));
+routeNames.forEach((name) => document.body.appendChild(createLink(name)));
 
-function renderPage(
-    page: () => void,
-) {
-    clean();
-    page();
+function renderPage(page: () => void) {
+  clean();
+  page();
 }
 
 switch (location.pathname) {
-    case createHref(routeNames[0]):
-        renderPage(helloWorld);
-        break;
-    case createHref(routeNames[1]):
-        renderPage(treasureMap);
-        break;
-    case createHref(routeNames[2]):
-        renderPage(canvasBasics);
-        break;
-    case createHref(routeNames[3]):
-        renderPage(canvasMovement);
-        break;
-    case createHref(routeNames[4]):
-        renderPage(canvasKeyboard);
-        break;
-    case createHref(routeNames[5]):
-        renderPage(snake);
-        break;
+  case createHref(routeNames[0]):
+    renderPage(helloWorld);
+    break;
+  case createHref(routeNames[1]):
+    renderPage(treasureMap);
+    break;
+  case createHref(routeNames[2]):
+    renderPage(canvasBasics);
+    break;
+  case createHref(routeNames[3]):
+    renderPage(canvasMovement);
+    break;
+  case createHref(routeNames[4]):
+    renderPage(canvasKeyboard);
+    break;
+  case createHref(routeNames[5]):
+    renderPage(snake);
+    break;
+  case createHref(routeNames[6]):
+    renderPage(platformGame);
+    break;
 }
